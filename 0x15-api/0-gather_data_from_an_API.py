55e3b46825237a9f1fa35e06dbaeca8422adf3b3
betiniakarandut <betiniakarandut@gmail.com>
2023-05-25 22:42:21 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Script that gathers information from an API
+import requests
+import sys as s
+
+if __name__ == "__main":
+    employee_id = s.argv[1]
+    userId = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+    name = user.json().get('name')
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/users/todos")
+    totalTask = 0
+    completed = 0
+    for tasks in todos.json():
+        if task.get('userId') == int(userId):
+            totaltask += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(name, completed, totalTask))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get("userId") == int(userId) and task.get('completed')]))
+
+
