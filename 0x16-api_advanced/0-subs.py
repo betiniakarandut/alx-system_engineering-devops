b9e1dabc5601b02be1fcc936ddfcef34f01b6d37
betiniakarandut <betiniakarandut@gmail.com>
2023-06-07 04:33:21 +0000

    Subreddit: number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,10 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """Module to find the number of subcribers on reddit"""
+import requests
+import sys as s
 
-def def number_of_subscribers(subreddit):
+
+def number_of_subscribers(subreddit):
     """function that queries the Reddit API.
     
     Args:
@@ -9,11 +12,12 @@ def def number_of_subscribers(subreddit):
 
     Return: number of subscribers
     """
-    import requests
-    info = requests.get("https://www.reddit.com/r/{}/about.json"
-                        .format(subreddit),
-                        headers = {"User-Agent": "My-User-Agent"}.
-                        allow_redirects=False)
-    if info.status_code >= 300:
+    subreddit = s.argv[1]
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers={"User-Agent": "MyBot"})
+    if response.status_code >= 300:
         return (0)
-    return info/json().get("data").get("subscribers") 
+    else:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
