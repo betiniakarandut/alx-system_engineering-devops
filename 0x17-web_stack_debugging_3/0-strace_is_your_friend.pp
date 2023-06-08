bf84f7a578df8081586c4ffdebb81d39af88c1e1
betiniakarandut <betiniakarandut@gmail.com>
2023-06-08 04:42:45 +0000

    Removed apostrophe

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet code to fix 500 error from wordpress website
 exec { 'fix-wordpress':
-  command =>'sed -i 's/phhp/php/g' var/www/html/wp-setting.php' sudo systemctl restart apache2
+  command =>'sed -i s/phhp/php/g var/www/html/wp-setting.php; sudo systemctl restart apache2'
   path    => ['/bin', '/usr/bin', '/bin/sbin']
 }
