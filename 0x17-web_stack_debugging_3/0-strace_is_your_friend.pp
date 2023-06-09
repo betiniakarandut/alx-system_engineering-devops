6f1cf3af5beb4b165a90f380f737c92733070df5
betiniakarandut <betiniakarandut@gmail.com>
2023-06-09 12:13:18 +0000

    Correction: added a comma

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet code to fix 500 error from wordpress website
 exec { 'fix-wordpress':
-  command =>'sed -i s/phhp/php/g var/www/html/wp-setting.php; sudo systemctl restart apache2'
+  command =>'sed -i s/phpp/php/g var/www/html/wp-setting.php; sudo service apache2 restart',
   path    => ['/bin', '/usr/bin', '/bin/sbin']
 }
