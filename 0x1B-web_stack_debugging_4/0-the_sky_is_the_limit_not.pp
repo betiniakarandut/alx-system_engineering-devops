506855d8314757d98eaa8f3e3d8d9f9b39f511d2
betiniakarandut <betiniakarandut@gmail.com>
2023-06-15 12:29:15 +0000

    HTTP request fix

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# puppet script to minize HTTP request error to zero.
+exec { 'nginx-http-fix':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
