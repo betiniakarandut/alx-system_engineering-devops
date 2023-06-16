e36391e8436b6e8a3d7df4ded7acd5bdccc4e37c
betiniakarandut <betiniakarandut@gmail.com>
2023-06-16 04:48:47 +0100

    Change code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Recursive module to querry reddit api"""
+import requests
+import sys
 
 
-def recurse(subreddit, hot_list=[], count=0, after=None):
+def add_title(hot_list, hot_posts):
     """prints a sorted count of given keywords
 
     Args:
@@ -13,24 +15,37 @@ def recurse(subreddit, hot_list=[], count=0, after=None):
     Returns:
         recursively a list containing the titles of all hot articles
     """
-    import requests
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
 
-    res = requests.get("https://www.reddit.com/r/{}/hot.json"
-                       .format(subreddit),
-                       params={"count": count, "after": after},
-                       headers={"User-Agent": "My-User-Agent"},
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
                        allow_redirects=False)
-    if res.status_code >= 400:
+
+    if res.status_code != 200:
         return None
 
-    hot_l = hot_list + [child.get("data").get("title")
-                        for child in res.json()
-                        .get("data")
-                        .get("children")]
-
-    feedback = res.json()
-    if not feedback.get("data").get("after"):
-        return hot_l
-
-    return recurse(subreddit, hot_l, feedback.get("data").get("count"),
-                   feedback.get("data").get("after"))
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
